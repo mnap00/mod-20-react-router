239e17ffa8db43c82bfa3a953a5ce50f2e307df5
mnap <learn@10g.pl>
2018-05-17 21:32:27 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+
+//ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<h1>Test Page</h1>, document.getElementById('root'));
+registerServiceWorker();
